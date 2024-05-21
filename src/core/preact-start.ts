9eac8cf02f43b1ce45d98c3abe7ffba1444026be
Sebastián García <ia3266505@gmail.com>
2024-05-21 09:32:02 -0500

    Update folders core preact-start (#6)

---
+++ src/core/preact-start.ts
@@ -0,0 +1,8 @@
+/**
+ * Author: Sebastian Garcia
+ * This file is used to start the preact application
+ * MIT License
+ * @file src/core/preact-start.ts
+ * @version 0.1.0
+ */
+export const start = 'preact-start'
